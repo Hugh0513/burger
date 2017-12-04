324d643401bfe114db84b63c94085207440014dc
Ryosaku <hiwaryo@gmail.com>
2017-12-03 23:35:45 -0800

    better

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('protein burger', false);
